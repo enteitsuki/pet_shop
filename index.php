a3ee7f6e9fa0a3edd6970de9e9e748155b719031
enteitsuki <enteitsuki@gmail.com>
2020-11-07 23:21:05 +0900

    ペットショップアプリ課題1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=pet_shop;charset=utf8;');
+define('USER', 'staff');
+define('PASSWORD', '9999');
+
+try {
+    $dbh = new PDO(DSN, USER, PASSWORD);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+$sql = 'SELECT * FROM animals';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($animals as $animal) {
+    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん<br>';
+    echo $animal['description'] . '<br>';
+    echo $animal['birthday'] . '生まれ<br>';
+    echo '出身地' . $animal['birthplace'] . '<hr>';
+}
