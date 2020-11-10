de4252363916401723b7e691463ce310840ec43c
enteitsuki <enteitsuki@gmail.com>
2020-11-10 20:10:25 +0900

    ペットショップアプリ課題2

--- index.php
+++ index.php
@@ -13,13 +13,47 @@ try {
 
 $sql = 'SELECT * FROM animals';
 $stmt = $dbh->prepare($sql);
+
 $stmt->execute();
 
-$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$keyword = $_POST["keyword"];
+if ($keyword != '') {
+    $sql2 = 'SELECT * FROM animals WHERE description LIKE :keyword';
+    $stmt = $dbh->prepare($sql2);
 
-foreach ($animals as $animal) {
-    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' .
-         $animal['description'] . '<br>' .
-         $animal['birthday'] . '生まれ' . '<br>' .
-         '出身地' . $animal['birthplace'] . '<hr>';
+    $stmt->bindParam(':keyword', $keyword, PDO::PARAM_STR);
+    $keyword = '%' . $keyword . '%';
+
+    $stmt->execute();
 }
+
+$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ペットショップアプリ課題2</title>
+</head>
+
+<body>
+    <h2>本日のご紹介のペット！</h2>
+
+    <form action="index.php" method="post">
+        キーワード: <input type="text" name="keyword" placeholder="キーワードの入力">
+        <input type="submit" value="検索">
+    </form>
+
+    <?php foreach ($animals as $animal) {
+        echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' .
+            $animal['description'] . '<br>' .
+            $animal['birthday'] . '生まれ' . '<br>' .
+            '出身地' . $animal['birthplace'] . '<hr>';
+    }
+    ?>
+</body>
+
+</html>
